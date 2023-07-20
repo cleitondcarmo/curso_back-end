d528516502fc13ae196784a9d6df4d4d8cc7d5bd
cleitondcarmo <cleitondcarmo@hotmail.com>
2023-07-20 02:11:10 -0300

    curso back-end

---
+++ src/entities/product.entity.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty } from "class-validator";
+import { Length} from 'class-validator';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
+import { v4 as uuid } from 'uuid'
+
+@Entity('products')
+export class Product {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  weight: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid()
+    }
+  }
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,24 @@
+import {Request, Response, Router} from "express"
+import ProductController from '@/controllers/product.controller'
+
+const routes = Router()
+
+routes.get('/api/products', ProductController.findAll)
+routes.post('/api/products', ProductController.create)
+routes.get('/api/products/:id', ProductController.findOne)
+routes.put('/api/products/:id', ProductController.update)
+routes.delete('/api/products/:id', ProductController.delete)
+
+routes.get('/', (_:Request, response: Response) => {
+  response.status(200).send({
+    sucess: true
+  })
+})
+
+routes.get("*" , (_:Request, response: Response) => {
+  response.status(404).send({
+    error: "Not Found"
+  })
+})
+
+export default routes
